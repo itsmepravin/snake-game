a6929f8740f4e5bade8a40af20e8b431c0cc93e6
itsmepravin <singh.pravin19@yahoo.com>
2024-01-02 16:47:33 +0545

    All files pushed

---
+++ snake.js
@@ -0,0 +1,76 @@
+var blockSize = 25;
+var rows = 20;
+var columns = 20;
+var canvas;
+var context;
+
+// Snake
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+// Food
+var foodX;
+var foodY;
+
+var velocityX = 0;
+var velocityY = 0;
+
+window.onload = function () {
+  canvas = document.getElementById("canvas");
+  canvas.width = columns * blockSize;
+  canvas.height = rows * blockSize;
+  context = canvas.getContext("2d");
+
+  document.addEventListener("keyup", changeSnakeDirection);
+
+  placeFood();
+  setInterval(update, 1000 / 10);
+  // update();
+};
+
+function update() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "red";
+  context.fillRect(foodX, foodY, blockSize, blockSize);
+
+  if (snakeX == foodX && snakeY == foodY) {
+    placeFood();
+  }
+
+  context.fillStyle = "lime";
+  snakeX += velocityX * blockSize;
+  snakeY += velocityY * blockSize;
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      const x = i * blockSize;
+      const y = j * blockSize;
+      context.strokeStyle = "blue";
+      context.strokeRect(x, y, blockSize, blockSize);
+    }
+  }
+}
+
+function placeFood() {
+  foodX = Math.floor(Math.random() * columns) * blockSize;
+  foodY = Math.floor(Math.random() * rows) * blockSize;
+}
+
+function changeSnakeDirection(e) {
+  if (e.code == "ArrowUp") {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.code == "ArrowDown") {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.code == "ArrowLeft") {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.code == "ArrowRight") {
+    velocityX = 1;
+    velocityY = 0;
+  }
+}
